9f3e9bf8d6e00d07164e972990640ae7609eb7c7
MichaelMantovani <michmantovani@gmail.com>
2023-11-21 09:50:10 +0100

    create Evento class with getter and setter

--- src/org/java/pojo/Evento.java
+++ src/org/java/pojo/Evento.java
@@ -1,5 +1,46 @@
 package org.java.pojo;
 
-public class Evento {
+import java.time.LocalTime;
 
+public class Evento {
+	private String title;
+	private LocalTime date;
+	private int totalSeats;
+	static int reservedSeats = 0;
+	
+	public Evento(String title, LocalTime date, int totalSeats) {
+			setTitle(title);
+			setDate(date);
+			setTotalSeats(totalSeats);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalTime date) {
+		this.date = date;
+	}
+
+	public int getTotalSeats() {
+		return totalSeats;
+	}
+	
+	private void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+
+	public int getReservedSeats () {
+		return reservedSeats;
+	}
+	
+	
 }
