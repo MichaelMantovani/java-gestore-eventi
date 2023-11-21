6af2d25ec4007bb804c99184720548e01531bc21
MichaelMantovani <michmantovani@gmail.com>
2023-11-21 10:35:07 +0100

    add Exception in Evento Class

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,7 +1,26 @@
 package org.java;
 
+import org.java.pojo.Evento;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
+	
+		
+		try {
+			Evento matrimonio = new Evento("Matrimonio", "23/09/2023 11:00:00", 0);
+			System.out.println(matrimonio);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());;
+		}
+		
+		
+		
+		
+		
+	
+		
 
 	}
+	
+	
 }
--- src/org/java/pojo/Evento.java
+++ src/org/java/pojo/Evento.java
@@ -1,17 +1,21 @@
 package org.java.pojo;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
+
 	private String title;
-	private LocalTime date;
+	private LocalDateTime date;
 	private int totalSeats;
-	static int reservedSeats = 0;
-	
-	public Evento(String title, LocalTime date, int totalSeats) {
-			setTitle(title);
-			setDate(date);
-			setTotalSeats(totalSeats);
+	private static int reservedSeats = 0;
+	private static final LocalDateTime today = LocalDateTime.now();
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+	public Evento(String title, String date, int totalSeats) throws Exception {
+		setTitle(title);
+		setDate(date);
+		setTotalSeats(totalSeats);
 	}
 
 	public String getTitle() {
@@ -22,25 +26,37 @@ public class Evento {
 		this.title = title;
 	}
 
-	public LocalTime getDate() {
+	public LocalDateTime getDate() {
 		return date;
 	}
 
-	public void setDate(LocalTime date) {
-		this.date = date;
+	public void setDate(String date) throws Exception {
+		LocalDateTime dateTime = LocalDateTime.parse(date, dtf);
+		if (dateTime.compareTo(today) >= 0)
+			throw new Exception("**Inserisci una data valida**");
+		else 
+			this.date = dateTime;
 	}
 
 	public int getTotalSeats() {
 		return totalSeats;
 	}
-	
-	private void setTotalSeats(int totalSeats) {
-		this.totalSeats = totalSeats;
+
+	private void setTotalSeats(int totalSeats) throws Exception {
+		if (totalSeats <= 0)
+			throw new Exception("**Deve esserci almeno un posto**");
+		else
+			this.totalSeats = totalSeats;
 	}
 
-	public int getReservedSeats () {
+	public int getReservedSeats() {
 		return reservedSeats;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "L'evento " + this.title + " si terrà il " + dtf.format(this.date) + "\n"
+				+ "Il numero di posti totali è " + this.totalSeats + " mentre le prenotazione sono " + reservedSeats;
+	}
+
 }
