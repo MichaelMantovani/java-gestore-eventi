26ec215db33b5f39c2e4de5ce9dccd8f7dde437b
MichaelMantovani <michmantovani@gmail.com>
2023-11-21 12:45:07 +0100

    add price format in Concerto class

--- src/org/java/pojo/Concerto.java
+++ src/org/java/pojo/Concerto.java
@@ -2,20 +2,21 @@ package org.java.pojo;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.text.NumberFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class Concerto extends Evento{
+public class Concerto extends Evento {
 	private LocalTime time;
 	private BigDecimal price;
 	static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
-	
+
 	public Concerto(String title, String date, int totalSeats, String time, double price) throws Exception {
 		super(title, date, totalSeats);
 		setTime(time);
 		setPrice(price);
 	}
-	
+
 	public LocalTime getTime() {
 		return time;
 	}
@@ -25,19 +26,19 @@ public class Concerto extends Evento{
 		this.time = eventTime;
 	}
 
-	public BigDecimal getPrice() {
-		return price;
+	public String getPrice() {
+		return NumberFormat.getCurrencyInstance().format(price);
 	}
 
 	public void setPrice(double price) {
-		BigDecimal eventPrice = new BigDecimal(price,MathContext.DECIMAL32 );
+		BigDecimal eventPrice = new BigDecimal(price, MathContext.DECIMAL32);
 		this.price = eventPrice;
 	}
 
 	@Override
 	public String toString() {
-		
-		return super.toString()  + " alle " + this.time;
+
+		return super.toString() + " alle " + this.time + "\n" + "Il biglietto costa " + getPrice();
 	}
-	
+
 }
