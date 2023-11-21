ef143322f9c0ed339748a7ded905473fabc7d2e5
MichaelMantovani <michmantovani@gmail.com>
2023-11-21 12:48:58 +0100

    finish milestone3

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -7,75 +7,78 @@ import org.java.pojo.Evento;
 
 public class Main {
 	public static void main(String[] args) {
-//		Scanner in = new Scanner(System.in);
-//
-//		System.out.println("Vuoi creare un nuovo evento (si/no)?");
-//		boolean newEvent = in.nextLine().equals("si") ? true : false;
-//		if (newEvent) {
-//			boolean retry = false;
-//			Evento evento = null;
-//
-////			Chiedo all'utente tutte le informazioni per creare un evento e provo a crearlo
-//			do {
-//				System.out.println("Qual è il titolo dell'evento? ");
-//				String title = in.nextLine();
-//				System.out.println("In che giorno e a che ora si terrà? (Es. 23/10/2024 11:00:00)");
-//				String dateTime = in.nextLine();
-//				System.out.println("Quanti sono i posti totali disponibili");
-//				int totalSeats = Integer.valueOf(in.nextLine());
-//
-//				try {
-//					evento = new Evento(title, dateTime, totalSeats);
-//					retry = false;
-//				} catch (Exception e) {
-//					System.out.println(e.getMessage());
-//					retry = true;
-//				}
-//			} while (retry);
-//
-////			Chiedo all'utente quante prenotazioni vuole effettuare e provo ad effettuarle
-//			do {
-//				System.out.println("Quante prenotazione vuoi effettuare?");
-//				int totalReservation = Integer.valueOf(in.nextLine());
-//				try {
-//					evento.prenota(totalReservation);
-//					System.out.println("Posti prenotati :" + evento.getReservedSeats() + "\n" + "Posti disponibili: "
-//							+ evento.getTotalSeats());
-//					retry = false;
-//				} catch (Exception e) {
-//					System.out.println(e.getMessage());
-//					retry = true;
-//				}
-//			} while (retry);
-//
-////			Chiedo all'utente se e quante prenotazione vuole disdire e provo a disdirle
-//			System.out.println("Vuoi disdire delle prenotazioni? (si/no)");
-//			boolean cancellationEnabled = in.nextLine().equals("si") ? true : false;
-//			if (cancellationEnabled) {
-//				do {
-//					System.out.println("Quante prenotazioni vuoi disdire?");
-//					int deleteReservation = Integer.valueOf(in.nextLine());
-//					try {
-//						evento.disdici(deleteReservation);
-//						System.out.println("Posti prenotati :" + evento.getReservedSeats() + "\n"
-//								+ "Posti disponibili: " + evento.getTotalSeats());
-//						retry = false;
-//					} catch (Exception e) {
-//						System.out.println(e.getMessage());
-//						retry = true;
-//					}
-//				} while (retry);
-//			} else
-//				System.out.println("Arrivederci");
-//
-//		} else
-//			System.out.println("Arrivederci");
-//
+		Scanner in = new Scanner(System.in);
+		
+//		Provo di creare un concerto
 		try {
-			Concerto metallica = new Concerto("Metallica", "23/10/2024", 1500, "11:00", 11.50);
+			Concerto metallica = new Concerto("Metallica on tour", "23/10/2024", 20000, "11:00", 50);
 			System.out.println(metallica);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
+//		Chiedo all'untete se vuole creare un evento, se si gli chiedo tutte le informazioni necessarie
+		System.out.println("Vuoi creare un nuovo evento (si/no)?");
+		boolean newEvent = in.nextLine().equals("si") ? true : false;
+		if (newEvent) {
+			boolean retry = false;
+			Evento evento = null;
+
+//			Chiedo all'utente tutte le informazioni per creare un evento e provo a crearlo
+			do {
+				System.out.println("Qual è il titolo dell'evento? ");
+				String title = in.nextLine();
+				System.out.println("In che giorno e a che ora si terrà? (Es. 23/10/2024 11:00:00)");
+				String dateTime = in.nextLine();
+				System.out.println("Quanti sono i posti totali disponibili");
+				int totalSeats = Integer.valueOf(in.nextLine());
+
+				try {
+					evento = new Evento(title, dateTime, totalSeats);
+					retry = false;
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					retry = true;
+				}
+			} while (retry);
+
+//			Chiedo all'utente quante prenotazioni vuole effettuare e provo ad effettuarle
+			do {
+				System.out.println("Quante prenotazione vuoi effettuare?");
+				int totalReservation = Integer.valueOf(in.nextLine());
+				try {
+					evento.prenota(totalReservation);
+					System.out.println("Posti prenotati :" + evento.getReservedSeats() + "\n" + "Posti disponibili: "
+							+ evento.getTotalSeats());
+					retry = false;
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					retry = true;
+				}
+			} while (retry);
+
+//			Chiedo all'utente se e quante prenotazione vuole disdire e provo a disdirle
+			System.out.println("Vuoi disdire delle prenotazioni? (si/no)");
+			boolean cancellationEnabled = in.nextLine().equals("si") ? true : false;
+			if (cancellationEnabled) {
+				do {
+					System.out.println("Quante prenotazioni vuoi disdire?");
+					int deleteReservation = Integer.valueOf(in.nextLine());
+					try {
+						evento.disdici(deleteReservation);
+						System.out.println("Posti prenotati :" + evento.getReservedSeats() + "\n"
+								+ "Posti disponibili: " + evento.getTotalSeats());
+						retry = false;
+					} catch (Exception e) {
+						System.out.println(e.getMessage());
+						retry = true;
+					}
+				} while (retry);
+			} else
+				System.out.println("Arrivederci");
+
+		} else
+			System.out.println("Arrivederci");
+		in.close();
 	}
 }
